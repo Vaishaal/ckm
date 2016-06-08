a4ea1e7278b74f209820f22975e358db22fbf951
Vaishaal Shankar <vaishaal@berkeley.edu>
2016-06-08 16:13:00 -0700

    Log in a better place

--- keystone_pipeline/bin/run-deep-imagenet.sh
+++ keystone_pipeline/bin/run-deep-imagenet.sh
@@ -4,5 +4,5 @@ FWDIR="$(cd `dirname $0`/..; pwd)"
 
 DATE=`date +"%Y_%m_%d_%H_%M_%S"`
 
-$FWDIR/bin/run-pipeline-yarn.sh pipelines.CKMDeepImageNet $FWDIR/target/scala-2.10/ckm-assembly-0.1.jar "$@" 2>&1 | tee /home/eecs/vaishaal/deep-ckm-$DATE.log
+$FWDIR/bin/run-pipeline-yarn.sh pipelines.CKMDeepImageNet $FWDIR/target/scala-2.10/ckm-assembly-0.1.jar "$@" 2>&1 | tee /home/eecs/vaishaal/logs/deep-ckm-$DATE.log
 
